58ee07d26161e395c5c7784b35a6e2fd6d965c06
EduardoLopesR <edulopesresende19@gmail.com>
2026-02-06 10:58:32 -0300

    Aula 2 JS
    Atividade referente à criação de um aplicativo simples de cadeia seguindo as seguintes orientações:
    Criar um pequeno programa em JavaScript onde o usuário insere seus dados pessoais (nome, idade e cidade) e o sistema exibe essas informações formatadas.
    Criar três variáveis (let nome, let idade, let cidade).Solicitar que o usuário insira valores através do prompt().Exibir uma mensagem formatada usando console.log() ou alert().

---
+++ aula2.js
@@ -0,0 +1,8 @@
+let nome = prompt("Digite seu nome:");
+let idade = prompt("Digite sua idade:");
+let cidade = prompt("Digite sua cidade:");
+
+let mensagem = `--- Dados do Usuário ---\nNome: ${nome}\nIdade: ${idade}\nCidade: ${cidade}`;
+
+console.log(mensagem); 
+alert(mensagem); 
