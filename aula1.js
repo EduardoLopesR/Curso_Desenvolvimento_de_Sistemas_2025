f9109cceb0fa005b4dc0f1e4e7b72a076b0867d9
EduardoLopesR <edulopesresende19@gmail.com>
2026-02-06 09:36:36 -0300

    Add files via upload
    Atividade do curso referente à JavaScript com objetivo de criar uma página interativa seguindo as seguintes aplicações:
    -Um botão altera a cor de fundo da tela ao ser clicado para verde.
    -Um botão altera a cor de fundo da tela ao ser clicado para vermelho.
    -Um botão para retornar a cor original da tela ao ser clicado.
    -Um botão para apresentar uma mensagem: "Olá, seja bem-vindo ao meu app da aula 01 Introdução ao JavaScript";

---
+++ aula1.js
@@ -0,0 +1,13 @@
+
+function trocaVerde(){
+    document.body.style.backgroundColor = 'green'
+}
+function trocaVermelho(){
+    document.body.style.backgroundColor = 'red'
+}
+function trocaPadrao(){
+    document.body.style.backgroundColor = ''
+}
+function texto(){
+    document.getElementById("meuTexto").style.display = "block";
+}
